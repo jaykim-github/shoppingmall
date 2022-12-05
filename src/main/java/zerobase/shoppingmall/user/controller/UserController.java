2ac10dfb407dc5cdc194999eeb27fdb542df7d55
jaykim-github <jaykeem713@gmail.com>
2022-12-05 23:05:34 +0900

    test PR 테스트

--- src/main/java/zerobase/shoppingmall/user/controller/UserController.java
+++ src/main/java/zerobase/shoppingmall/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package zerobase.shoppingmall.user.controller;
 
-import jodd.net.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +10,7 @@ import zerobase.shoppingmall.user.dto.UserInput;
 import zerobase.shoppingmall.user.service.UserService;
 
 @RestController
-public class UserController {
-
+public class UserController {  //PR테스트
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -25,18 +23,18 @@ public class UserController {
     }
 
     @GetMapping("/user/email_auth")
-    public BaseResponse emailAuth(@RequestParam String id){
+    public BaseResponse emailAuth(@RequestParam String id) {
         String uuid = id;
 
         return userService.emailAuth(uuid);
     }
 
     @PostMapping("/login")
-    public BaseResponse logiin(@RequestBody UserInput userInput) {
+    public BaseResponse login(@RequestBody UserInput userInput) {
         String user_id = userInput.getUser_id();
         String password = userInput.getPassword();
 
-        return userService.login(user_id,password);
+        return userService.login(user_id, password);
     }
 
 }
